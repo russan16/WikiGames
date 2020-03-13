ac63c71b4459bffd269a40d953eb89d9188d0e56
Italo Russan <italo.russan@csktech.com.br>
2020-03-13 17:05:45 -0300

    feat: cor na contagem de jogos por plataforma

--- wikigames/src/Pages/Plataformas/index.js
+++ wikigames/src/Pages/Plataformas/index.js
@@ -20,7 +20,7 @@ export default class Plataformas extends Component {
             <div className="mt-5 mb-5 flex-wrap">
                 <h1 className="display-4 text-light mb-5">Jogos por plataforma</h1>
                 {this.state.plataformas.map(plat =>(
-                    <a href={`/plataforma-games/${plat.id}/${plat.name}`} className="btn btn-secondary mr-2 mb-2">{plat.name} <span className="badge badge-primary badge-pill">{plat.games_count}</span></a>
+                    <a href={`/plataforma-games/${plat.id}/${plat.name}`} className="btn btn-secondary mr-2 mb-2">{plat.name} <span className="badge badge-dark badge-pill">{plat.games_count}</span></a>
                 ))}
             </div>
         );
