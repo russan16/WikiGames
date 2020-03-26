e90b8e2a9f04d6b2a9bde1d69a099af0db9dccad
Italo Russan <italo.russan@csktech.com.br>
2020-03-26 14:53:39 -0300

    fix frase do header

--- src/Pages/Interna/index.js
+++ src/Pages/Interna/index.js
@@ -26,7 +26,7 @@ export default class Interna extends Component {
             <div className="game-info">
                 <h1 className="display-3 text-light w-100 text-center mt-5 mb-3">{product.name}</h1>
                 <div className="text-light">
-                    <figure className="img-thumbnail img-info float-left mr-4">
+                    <figure className="img-thumbnail img-info float-md-left mr-md-4 mr-0">
                         <img src={product.background_image_additional} alt={product.name}/>
                     </figure>
                     <p>{product.description_raw}</p>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,7 @@ import ImgHeader from '../../imgs/wikigames-header.jpg';
 
 const Header = () => (
     <header className="row position-relative d-flex justify-content-center align-items-center">
-        <h3 className="text-header display-1 text-light text-center d-flex flex-column position-absolute"> WikiGames <small className="h6"> Sua
-            fonte
-            de
-            informações
-            sobre
-            games.
-        </small></h3>
+        <h3 className="text-header display-1 text-light text-center d-flex flex-column position-absolute"> WikiGames <small className="h6">A Wiki de todo Gamer</small></h3>
         <figure className="figure header-img overflow-hidden d-flex justify-content-center align-items-center">
             <img className="img-fluid dark-img" src={ImgHeader} alt="Capa do site"/>
         </figure>
