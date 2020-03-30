5e0600f6242cd5d0506d196d4485e6ed2bb0a4cc
Italo Russan <italo.russan@csktech.com.br>
2020-03-30 11:50:42 -0300

    update: dados da meta do site

--- src/Pages/Plataformas/index.js
+++ src/Pages/Plataformas/index.js
@@ -21,7 +21,7 @@ export default class Plataformas extends Component {
             <div className="mt-5 mb-5 flex-wrap">
                 <h1 className="display-4 text-light mb-5">Jogos por plataforma</h1>
                 {this.state.plataformas.map(plat =>(
-                    <Link to={`/plataforma-games/${plat.id}/${plat.name}`} className="btn btn-secondary mr-2 mb-2">{plat.name} <span className="badge badge-dark badge-pill">{plat.games_count}</span></Link>
+                    <Link to={`/plataforma-games/${plat.id}/${plat.name}`} key={plat.id} className="btn btn-secondary mr-2 mb-2">{plat.name} <span className="badge badge-dark badge-pill">{plat.games_count}</span></Link>
                 ))}
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './styles.scss';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
